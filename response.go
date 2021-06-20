abb1017550b96fc41f77fa1d2e8caf815e2bd4d7
Tamal Saha <tamal@appscode.com>
2021-06-20 02:44:15 -0700

    Add APIError to response (#2)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- response.go
+++ response.go
@@ -25,13 +25,18 @@ type ResponseWriter interface {
 	XML(status int, v interface{})
 	Error(status int, contents ...string)
 
-	// misc render/response functions
+	// extended ResponseWriter methods
 	Written() bool
 	// Status returns the HTTP status of the request, or 0 if one has not
 	// yet been sent.
 	Status() int
 	// BytesWritten returns the total number of bytes sent to the client.
 	BytesWritten() int
+
+	// API error
+	APIError(err error) int
+
+	// misc render/response functions
 	Redirect(location string, status ...int)
 	RedirectToFirst(appURL, appSubURL string, location ...string)
 	HTMLString(name string, binding interface{}, htmlOpt ...render.HTMLOptions) (string, error)
@@ -65,25 +70,33 @@ func (w *response) TemplateLookup(t string) *template.Template {
 
 func (w *response) Render(e render.Engine, data interface{}) {
 	if err := w.r.Render(w, e, data); err != nil {
-		http.Error(w, fmt.Sprintf("Render failed, reason: %v", err), http.StatusInternalServerError)
+		if log := middleware.GetLogEntry(w.R().Request()); log != nil {
+			log.Panic(err.Error(), nil)
+		}
 	}
 }
 
 func (w *response) Data(status int, v []byte) {
 	if err := w.r.Data(w, status, v); err != nil {
-		http.Error(w, fmt.Sprintf("Render failed, reason: %v", err), http.StatusInternalServerError)
+		if log := middleware.GetLogEntry(w.R().Request()); log != nil {
+			log.Panic(err.Error(), nil)
+		}
 	}
 }
 
 func (w *response) HTML(status int, name string, binding interface{}, htmlOpt ...render.HTMLOptions) {
 	if err := w.r.HTML(w, status, name, binding, htmlOpt...); err != nil {
-		http.Error(w, fmt.Sprintf("Render failed, reason: %v", err), http.StatusInternalServerError)
+		if log := middleware.GetLogEntry(w.R().Request()); log != nil {
+			log.Panic(err.Error(), nil)
+		}
 	}
 }
 
 func (w *response) JSON(status int, v interface{}) {
 	if err := w.r.JSON(w, status, v); err != nil {
-		http.Error(w, fmt.Sprintf("Render failed, reason: %v", err), http.StatusInternalServerError)
+		if log := middleware.GetLogEntry(w.R().Request()); log != nil {
+			log.Panic(err.Error(), nil)
+		}
 	}
 }
 
@@ -101,7 +114,9 @@ func (w *response) Text(status int, v string) {
 
 func (w *response) XML(status int, v interface{}) {
 	if err := w.r.XML(w, status, v); err != nil {
-		http.Error(w, fmt.Sprintf("Render failed, reason: %v", err), http.StatusInternalServerError)
+		if log := middleware.GetLogEntry(w.R().Request()); log != nil {
+			log.Panic(err.Error(), nil)
+		}
 	}
 }
 
